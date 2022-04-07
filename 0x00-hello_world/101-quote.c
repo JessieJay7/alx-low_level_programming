a41a680bd3a1ae91dca5253732b924ce407af018
Gifty Wereko Danso Appiagyei <giftywdappiagyei@gmail.com>
2022-04-07 16:32:57 -0700

    Leraning to code in C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/**
-* 
+/** 
 * main - Entry point
 *
 * Return: Always 0 (Success)
@@ -10,7 +9,7 @@
 int main(void)
 {
 
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 			return (1);
 
