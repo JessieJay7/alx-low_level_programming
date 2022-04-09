661256eacc6774e9dac11e22c96e67a91b1e20c2
Gifty Wereko Danso Appiagyei <giftywdappiagyei@gmail.com>
2022-04-08 23:01:20 -0700

    Learning to code in C

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints 3 combination of numbers
+ * main - Prints all possible combinations of two-digit numbers,
+ * 	  ranging from 0-99, seperated by a comma followed by a space
  *
  * Return: Always (Success)
  */
