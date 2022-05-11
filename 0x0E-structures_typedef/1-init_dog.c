bb29816cdc0c0f990151f4b89c039adc0233cec6
Gifty Wereko Danso Appiagyei <giftywdappiagyei@gmail.com>
2022-05-11 13:56:07 -0700

    Learning structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,7 @@
  * Description: function that initialize a variable of type struct dog
 */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
