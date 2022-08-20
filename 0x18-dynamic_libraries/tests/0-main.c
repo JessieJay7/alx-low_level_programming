6f84fe03cf656535971a8f4df7b194ec82e39225
bright <obololastkiller@gmail.com>
2022-08-20 23:24:02 +0000

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/tests/0-main.c
@@ -0,0 +1,13 @@
+#include "../main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Alx students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("ALX SE"));
+	return (EXIT_SUCCESS);
+}
